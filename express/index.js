f8b86ee3e210373f6b0f135d7a18093b55bd9d31
Dainara Krewer <55606090+dainarakrewer@users.noreply.github.com>
2023-04-20 14:25:32 -0300

    20-04

---
+++ express/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.listen(4000, function (err){
+    if(err) {
+        console.log('Ocorreu um erro')
+    } else {
+        console.log('Servidor iniciado com sucesso')
+    }
+})
