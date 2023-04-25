14569fa8aa6432df0938ebcc6a7c3117669a6832
Dainara Krewer <55606090+dainarakrewer@users.noreply.github.com>
2023-04-25 15:20:33 -0300

    25-04

---
+++ GuiaPerguntas/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/:nome/:lang', (req, res) => {
+    var nome = req.params.nome
+    var lang = req.params.lang
+    var exibirMsg = false
+
+    var produtos = [
+        {nome: 'Teste', preco: 1.00},
+        {nome: 'Teste 1', preco: 2.00},
+        {nome: 'Teste 2', preco: 3.00},
+    ]
+
+    res.render('index', {
+        nome: nome,
+        lang: lang,
+        empresa: 'CloudCRM',
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos: produtos
+    })
+})
+
+app.listen(8080, () => {
+    console.log('App rodando!')
+})
